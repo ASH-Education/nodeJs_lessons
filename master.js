7f3f219954091c6c0ba069106163f45a7fab2364
AShraf Mohamed <124946844+ashrafmo-1@users.noreply.github.com>
2024-04-11 10:29:44 +0200

    edit redme file

--- master.js
+++ master.js
@@ -1,4 +1,32 @@
-console.log(
-  "hello world",
-  "hello from programming back end development"
-);
+const fs = require("node:fs"); // import file system module
+const crypto = require('node:crypto');
+
+// console.log("First line");
+// handle by libuv with thread pool
+// fs.readFile("./testing/test.txt", "utf8", (err, data) => {
+//   console.log("file content", data);
+// });
+
+const start = performance.now();
+
+// with Sync ===========================
+// crypto.pbkdf2Sync('secret', 'salt', 100000, 64, 'sha512');
+// console.log("end at : ", performance.now() - start);
+// crypto.pbkdf2Sync('secret', 'salt', 100000, 64, 'sha512');
+// console.log("end at : ", performance.now() - start);
+// crypto.pbkdf2Sync('secret', 'salt', 100000, 64, 'sha512');
+// console.log("end at : ", performance.now() - start);
+
+// without Sync ============thread pool===============
+// crypto.pbkdf2('secret', 'salt', 100000, 64, 'sha512', () => {
+// console.log("end at : ", performance.now() - start);
+// });
+// crypto.pbkdf2('secret', 'salt', 100000, 64, 'sha512', () => {
+// console.log("end at : ", performance.now() - start);
+// });
+// crypto.pbkdf2('secret', 'salt', 100000, 64, 'sha512', () => {
+// console.log("end at : ", performance.now() - start);
+// });
+// crypto.pbkdf2('secret', 'salt', 100000, 64, 'sha512', () => {
+// console.log("end at : ", performance.now() - start);
+// });
